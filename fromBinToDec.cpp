2fdfc9c8e2d8aab2be7fbc8dac69b1bd87b4c1e8
Alextwit <59707650+Alextwit@users.noreply.github.com>
2021-03-04 02:51:39 +0300

    Update fromBinToDec.cpp

--- fromBinToDec.cpp
+++ fromBinToDec.cpp
@@ -7,15 +7,18 @@ using namespace std;
 int main() 
 {
 	int result = 0;
-	int s, i, n, l;
+	int s, i, n;
+//	s = from char to int
+//	i = value index
+//	n = power of two
 	string num;
-	cout << "enter number: " << endl;
+	cout << "enter BINARY number: " << endl;
 	cin >> num;
 	for (i = 0, n = num.length() - 1; i < num.length(), n >= 0; i++, n--) {
 		s = num[i] - 48;
 		result += s *  pow(2, n);
 	}
-	cout << result << endl;
+	cout << "the resulting DECIMAL value: " << result << endl;
 	
 	return 0;
 }
