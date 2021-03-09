e1da0c97fcb534c7d56a2f1f2808835fd2e5efe2
Alextwit <59707650+Alextwit@users.noreply.github.com>
2021-03-10 00:07:15 +0300

    Update GuessTheNumber.cpp

--- GuessTheNumber.cpp
+++ GuessTheNumber.cpp
@@ -5,23 +5,23 @@
 using namespace std;
 
 int main() {
-	int num = 0, x;
+	int EnteredNumber = 0, HiddenNumber = 0;
 	
 	srand(time(NULL));
-	x = rand() % 100;
+	HiddenNumber = rand() % 100;
 	
 	cout << "it is a game GUESS THE NUMBER" << endl;
 	cout << "Computer thought of a number, try to guess it^^" << endl;
 			
-	while (num != x) {
+	while (EnteredNumber != HiddenNumber) {
 		cout << "enter the number: ";
-		cin >> num;
-		if (num < x) {
+		cin >> EnteredNumber;
+		if (EnteredNumber < HiddenNumber) {
 			cout << "number is greater, try again" << endl;
-		} else if (num > x) {
+		} else if (EnteredNumber > HiddenNumber) {
 			cout << "number is less, try again" << endl;
-		} if (num == x) {
-			cout << "you guessed" << endl;
+		} if (EnteredNumber == HiddenNumber) {
+			cout << "you guessed, grats" << endl;
 			break;
 		}
 	}
