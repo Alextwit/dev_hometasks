0eab1d738d59ba11d98fb9a3e15f4f680e17e999
Alextwit <59707650+Alextwit@users.noreply.github.com>
2021-03-15 16:19:43 +0300

    Add files via upload

---
+++ NumPowerFunction.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double power();
+
+double EnteringNumber, result = 1;
+int PowerToCount;
+
+int main () {
+	
+	cout << "enter num: ";
+	cin >> EnteringNumber;
+	cout << endl;
+	cout << "enter power: ";
+	cin >> PowerToCount;
+	cout << endl;
+	
+	power ();
+	
+	cout << "Answer: " << result << endl;
+	
+	return 0;
+}
+
+double power () {
+		
+	if (PowerToCount >= 0) {
+		for (int c = 0; c < PowerToCount; c++) {
+			result *= EnteringNumber;
+		}
+	} else {
+		for (int c = 0; c < abs(PowerToCount); c++) {
+			result *= 1 / EnteringNumber;
+		}
+	}
+	
+}
